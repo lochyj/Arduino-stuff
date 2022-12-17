52f4d23255d2ee28e91118ade8361da7bea68509
Lachlan Jowett <79903111+lochyj@users.noreply.github.com>
2022-12-17 13:10:06 +1100

    init

---
+++ main.ino
@@ -0,0 +1,21 @@
+#include <Stepper.h>
+
+#define STEPS 100
+
+Stepper stepper(STEPS, 8, 9, 10, 11);
+
+int previous = 0;
+
+void setup()
+{
+	stepper.setSpeed(1000);
+}
+
+void loop()
+{
+	int val = analogRead(0);
+
+    stepper.step(val - previous);
+
+    previous = val;
+}
